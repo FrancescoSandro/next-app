1f028b40fc40bb7b635c175cbd5b56cea42f58c8
FrancescoSandro <sandrobiswas36@gmail.com>
2026-07-13 21:25:03 +0600

    change proxy redirect URL

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,7 +6,7 @@ export default async function proxy(req: NextRequest) {
     const password = req.cookies.get("password")?.value;
 
     if (username !== "sandro" || password !== "105432") {
-        return NextResponse.redirect(new URL("/next-app", req.url));
+        return NextResponse.redirect(new URL("https://next-app-three-black.vercel.app/", req.url));
     }
     return NextResponse.next();
 }
